5c11ffeb2b72fdaa68b01e1464ceb5ec0967057f
jcThePants <jcthepants@gmail.com>
2014-10-26 09:47:00 -0700

    Warp implements INamedLocation, add JavaDoc comments

--- src/com/jcwhatever/bukkit/generic/warp/Warp.java
+++ src/com/jcwhatever/bukkit/generic/warp/Warp.java
@@ -1,5 +1,8 @@
 package com.jcwhatever.bukkit.generic.warp;
 
+import com.jcwhatever.bukkit.generic.mixins.INamedLocation;
+import com.jcwhatever.bukkit.generic.mixins.INamedLocationDistance;
+import com.jcwhatever.bukkit.generic.mixins.implemented.NamedLocationDistance;
 import com.jcwhatever.bukkit.generic.storage.IDataNode;
 import com.jcwhatever.bukkit.generic.utils.PreCon;
 import org.bukkit.Location;
@@ -7,11 +10,11 @@ import org.bukkit.Location;
 /**
  * Represents a warp location.
  */
-public class Warp {
+public class Warp implements INamedLocation {
 
-	private final String _warpName;
-	private final String _warpSearchName;
-	private final IDataNode _dataNode;
+    private final String _warpName;
+    private final String _warpSearchName;
+    private final IDataNode _dataNode;
     private Location _location;
 
     /**
@@ -21,44 +24,57 @@ public class Warp {
      * @param location  The warp location.
      * @param dataNode  The warp data node.
      */
-	public Warp (String warpName, Location location, IDataNode dataNode) {
-		_warpName = warpName;
-		_warpSearchName = warpName.toLowerCase();		
-		_location = location;
-		_dataNode = dataNode;
-	}
+    public Warp (String warpName, Location location, IDataNode dataNode) {
+        _warpName = warpName;
+        _warpSearchName = warpName.toLowerCase();
+        _location = location;
+        _dataNode = dataNode;
+    }
 
     /**
      * Get the name of the warp.
      */
-	public String getName() {
-		return _warpName;
-	}
+    @Override
+    public String getName() {
+        return _warpName;
+    }
 
     /**
      * Get the name of the warp in lowercase.
      */
-	public String getSearchName() {
-		return _warpSearchName;
-	}
+    @Override
+    public String getSearchName() {
+        return _warpSearchName;
+    }
 
     /**
      * Get the warp location.
      */
-	public Location getLocation() {
-		return _location;
-	}
+    @Override
+    public Location getLocation() {
+        return _location;
+    }
+
+    /**
+     * Get the distance from the warp to the specified location.
+     *
+     * @param location  The location to check.
+     */
+    @Override
+    public INamedLocationDistance getDistance(Location location) {
+        return new NamedLocationDistance(this, location);
+    }
 
     /**
      * Set the warp location.
      *
      * @param location  The warp location.
      */
-	public void setLocation(Location location) {
+    public void setLocation(Location location) {
         PreCon.notNull(location);
 
-		_location = location;
-		_dataNode.set(_warpName, location);
-		_dataNode.saveAsync(null);
-	}
+        _location = location;
+        _dataNode.set(_warpName, location);
+        _dataNode.saveAsync(null);
+    }
 }
--- src/com/jcwhatever/bukkit/generic/warp/WarpManager.java
+++ src/com/jcwhatever/bukkit/generic/warp/WarpManager.java
@@ -1,72 +1,105 @@
 package com.jcwhatever.bukkit.generic.warp;
 
+import com.jcwhatever.bukkit.generic.storage.IDataNode;
+import com.jcwhatever.bukkit.generic.utils.PreCon;
+import com.sun.istack.internal.Nullable;
+import org.bukkit.Location;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.bukkit.Location;
-
-import com.jcwhatever.bukkit.generic.storage.IDataNode;
-
+/**
+ * Manage warp locations.
+ */
 public class WarpManager {
 
-	private Map<String, Warp> _warpMap = new HashMap<String, Warp>();
-	private IDataNode _settings;
-	
-	public WarpManager (IDataNode settings) {
-		_settings = settings;
-		loadWarps();
-	}
-	
-	public Warp getWarp(String name) {
-		return _warpMap.get(name.toLowerCase());				
-	}
-	
-	public List<Warp> getWarps() {
-		return new ArrayList<Warp>(_warpMap.values());
-	}
-	
-	public boolean setWarp(String name, Location location) {
-		if (name == null || name.isEmpty() || location == null)
-			return false;
-		
-		Warp warp = getWarp(name);
-		
-		if (warp != null) {
-			warp.setLocation(location);
-		}
-		else {
-			warp = new Warp(name, location, _settings);
-			_settings.set(name, location);
-			_settings.saveAsync(null);
-			_warpMap.put(warp.getSearchName(), warp);
-		}
-		return true;
-	}
-	
-	public boolean deleteWarp(String name) {
-		Warp warp = getWarp(name);
-		if (warp == null)
-			return false;
-		
-		_warpMap.remove(warp.getSearchName());
-		_settings.set(warp.getName(), null);
-		_settings.saveAsync(null);
-		return true;
-	}
-	
-	private void loadWarps() {
-		Set<String> warpNames = _settings.getSubNodeNames();
-		if (warpNames == null)
-			return;
-		
-		for (String warpName : warpNames) {
-			Location location = _settings.getLocation(warpName);
-			Warp warp = new Warp(warpName, location, _settings);
-			_warpMap.put(warp.getSearchName(), warp);
-		}
-	}
-	
+    private Map<String, Warp> _warpMap = new HashMap<String, Warp>(10);
+    private IDataNode _settings;
+
+    /**
+     * Constructor.
+     *
+     * @param dataNode  The managers data node.
+     */
+    public WarpManager (IDataNode dataNode) {
+        _settings = dataNode;
+
+        loadSettings();
+    }
+
+    /**
+     * Get a warp by name.
+     *
+     * @param name  The name of the warp.
+     */
+    @Nullable
+    public Warp getWarp(String name) {
+        return _warpMap.get(name.toLowerCase());
+    }
+
+    /**
+     * Get all warps.
+     */
+    public List<Warp> getWarps() {
+        return new ArrayList<Warp>(_warpMap.values());
+    }
+
+    /**
+     * Set a warp location.
+     *
+     * @param name      The name of the warp.
+     * @param location  The warp location.
+     */
+    public boolean setWarp(String name, Location location) {
+        PreCon.notNullOrEmpty(name);
+        PreCon.notNull(location);
+
+        Warp warp = getWarp(name);
+
+        if (warp != null) {
+            warp.setLocation(location);
+        }
+        else {
+            warp = new Warp(name, location, _settings);
+            _settings.set(name, location);
+            _settings.saveAsync(null);
+            _warpMap.put(warp.getSearchName(), warp);
+        }
+        return true;
+    }
+
+    /**
+     * Delete a warp by name.
+     *
+     * @param name  The name of the warp.
+     */
+    public boolean deleteWarp(String name) {
+        PreCon.notNullOrEmpty(name);
+
+        Warp warp = getWarp(name);
+        if (warp == null)
+            return false;
+
+        _warpMap.remove(warp.getSearchName());
+        _settings.set(warp.getName(), null);
+        _settings.saveAsync(null);
+        return true;
+    }
+
+    // initial settings load
+    private void loadSettings() {
+        Set<String> warpNames = _settings.getSubNodeNames();
+        if (warpNames == null)
+            return;
+
+        for (String warpName : warpNames) {
+            Location location = _settings.getLocation(warpName);
+            Warp warp = new Warp(warpName, location, _settings);
+            _warpMap.put(warp.getSearchName(), warp);
+        }
+    }
+
 }
