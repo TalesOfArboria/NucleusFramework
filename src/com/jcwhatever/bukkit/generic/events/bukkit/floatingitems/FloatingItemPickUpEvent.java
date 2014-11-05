e31e040a11e95ec24dbd498e17cf91c2baadc427
jcThePants <jcthepants@gmail.com>
2014-11-04 18:17:12 -0800

    adapt code to modifications in GenericsLib events

--- src/com/jcwhatever/bukkit/generic/events/bukkit/floatingitems/FloatingItemPickUpEvent.java
+++ src/com/jcwhatever/bukkit/generic/events/bukkit/floatingitems/FloatingItemPickUpEvent.java
@@ -1,17 +1,17 @@
 /* This file is part of GenericsLib for Bukkit, licensed under the MIT License (MIT).
- *
+ * 
  * Copyright (c) JCThePants (www.jcwhatever.com)
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,35 +22,68 @@
  */
 
 
-package com.jcwhatever.bukkit.generic.events;
+package com.jcwhatever.bukkit.generic.events.bukkit.floatingitems;
 
-/**
- * The basis for all Generics events.
- */
-public abstract class AbstractGenericsEvent {
+import com.jcwhatever.bukkit.generic.items.floating.FloatingItem;
+import com.jcwhatever.bukkit.generic.utils.PreCon;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
+public class FloatingItemPickUpEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final FloatingItem _item;
+    private final Player _player;
     private boolean _isCancelled;
 
     /**
-     * Determine if the event can be cancelled.
-     */
-    public abstract boolean isCancellable();
-
-    /**
-     * Determine if the event is cancelled.
+     * Constructor.
      *
-     * <p>Always returns false if the event is not cancellable.</p>
+     * @param item    The item being despawned.
+     * @param player  The player picking up the item.
      */
-    public boolean isCancelled() {
-        return isCancellable() && _isCancelled;
+    public FloatingItemPickUpEvent(FloatingItem item, Player player) {
+        PreCon.notNull(item);
+        PreCon.notNull(player);
+
+        _item = item;
+        _player = player;
     }
 
     /**
-     * Set the cancelled state of the event.
-     *
-     * @param isCancelled  True to cancel the event.
+     * Get the floating item that is being despawned.
      */
+    public FloatingItem getFloatingItem() {
+        return _item;
+    }
+
+    /**
+     * Get the player who is picking up the item.
+     */
+    public Player getPlayer() {
+        return _player;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return _isCancelled;
+    }
+
+    @Override
     public void setCancelled(boolean isCancelled) {
         _isCancelled = isCancelled;
     }
 }
+
