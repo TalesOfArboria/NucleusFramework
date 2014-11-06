9e84bee9abc1d1f26cc578adbd38bbd423a20f83
jcThePants <jcthepants@gmail.com>
2014-11-06 14:13:09 -0800

    localize strings

--- src/com/jcwhatever/bukkit/generic/commands/AboutCommand.java
+++ src/com/jcwhatever/bukkit/generic/commands/AboutCommand.java
@@ -25,13 +25,13 @@
 
 package com.jcwhatever.bukkit.generic.commands;
 
-import org.bukkit.command.CommandSender;
-import org.bukkit.permissions.PermissionDefault;
-
 import com.jcwhatever.bukkit.generic.commands.arguments.CommandArguments;
 import com.jcwhatever.bukkit.generic.commands.exceptions.InvalidValueException;
+import com.jcwhatever.bukkit.generic.internal.Lang;
 import com.jcwhatever.bukkit.generic.messaging.Messenger;
-import com.jcwhatever.bukkit.generic.utils.TextUtils;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.PermissionDefault;
 
 @ICommandInfo(
         command={"about"},
@@ -41,23 +41,24 @@ import com.jcwhatever.bukkit.generic.utils.TextUtils;
         isHelpVisible=false)
 public class AboutCommand extends AbstractCommand {
 
+    static final String _PLUGIN_NAME = "{BOLD}{GREEN}{plugin-name} {plugin-version}";
+    static final String _AUTHOR = "Plugin by {plugin-author}";
+    static final String _HELP = "{AQUA}For a list of commands, type '/{plugin-command} ?'";
+
     @Override
     public void execute(CommandSender sender, CommandArguments args) throws InvalidValueException {
 
         Messenger.tell(_plugin, sender, "----------------------------------------");
 
-        String text = TextUtils.formatPluginInfo(_plugin, "{BOLD}{GREEN}{plugin-name} {plugin-version}");
-        Messenger.tell(_plugin, sender, text);
+        Messenger.tell(_plugin, sender, Lang.get(_plugin, _PLUGIN_NAME));
 
         if (_plugin.getDescription().getAuthors() != null &&
                 !_plugin.getDescription().getAuthors().isEmpty()) {
 
-            String author = TextUtils.formatPluginInfo(_plugin, "Plugin by {plugin-author}");
-            Messenger.tell(_plugin, sender, author);
+            Messenger.tell(_plugin, sender, Lang.get(_plugin, _AUTHOR));
         }
 
-        String list = TextUtils.formatPluginInfo(_plugin, "{AQUA}For a list of commands, type '/{plugin-command} ?'");
-        Messenger.tell(_plugin, sender, list);
+        Messenger.tell(_plugin, sender, Lang.get(_plugin, _HELP));
 
         Messenger.tell(_plugin, sender, "----------------------------------------");
     }
