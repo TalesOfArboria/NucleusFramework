306233df0fd3a614a45b706e39ca95c992c3d654
JCThePants <jcthepants@gmail.com>
2014-12-05 13:33:24 -0800

    add ChunkInfo and WorldInfo data objects
    ChunkBlockInfo implements IDataNodeSerializable and IGenericsSerializable

--- src/com/jcwhatever/bukkit/generic/regions/data/ChunkBlockInfo.java
+++ src/com/jcwhatever/bukkit/generic/regions/data/ChunkBlockInfo.java
@@ -24,20 +24,31 @@
 
 package com.jcwhatever.bukkit.generic.regions.data;
 
+import com.jcwhatever.bukkit.generic.file.GenericsByteReader;
+import com.jcwhatever.bukkit.generic.file.GenericsByteWriter;
+import com.jcwhatever.bukkit.generic.file.IGenericsSerializable;
+import com.jcwhatever.bukkit.generic.storage.IDataNode;
+import com.jcwhatever.bukkit.generic.storage.IDataNodeSerializable;
+import com.jcwhatever.bukkit.generic.storage.UnableToDeserializeException;
+
 import org.bukkit.Material;
+
+import java.io.IOException;
 
 /**
  * Data object to hold information about a single block
  */
-public final class ChunkBlockInfo implements Comparable<ChunkBlockInfo> {
+public final class ChunkBlockInfo implements Comparable<ChunkBlockInfo>,
+        IDataNodeSerializable, IGenericsSerializable {
 
-    private final int _x;
-    private final int _y;
-    private final int _z;
-    private final Material _material;
-    private final int _data;
-    private final int _light;
-    private final int _skylight;
+    private int _x;
+    private int _y;
+    private int _z;
+
+    private Material _material;
+    private int _data;
+    private int _light;
+    private int _skylight;
 
     /**
      * Constructor.
@@ -48,7 +59,8 @@ public final class ChunkBlockInfo implements Comparable<ChunkBlockInfo> {
      * @param y            The blocks Y coordinates.
      * @param chunkBlockZ  The blocks Z coordinates relative to its chunk,
      */
-    public ChunkBlockInfo(int chunkBlockX, int y, int chunkBlockZ, Material material, int data, int light, int skylight) {
+    public ChunkBlockInfo(int chunkBlockX, int y, int chunkBlockZ, Material material,
+                          int data, int light, int skylight) {
         _x = chunkBlockX;
         _y = y;
         _z = chunkBlockZ;
@@ -107,6 +119,25 @@ public final class ChunkBlockInfo implements Comparable<ChunkBlockInfo> {
         return _z;
     }
 
+    @Override
+    public int hashCode() {
+        return _x ^ _y ^ _z;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof ChunkBlockInfo) {
+            ChunkBlockInfo other = (ChunkBlockInfo)obj;
+
+            return other._x == _x &&
+                    other._y == _y &&
+                    other._z == _z;
+        }
+
+        return false;
+    }
+
     /**
      * Sort by Y coordinates. Lowest to Highest
      */
@@ -114,5 +145,52 @@ public final class ChunkBlockInfo implements Comparable<ChunkBlockInfo> {
     public int compareTo(ChunkBlockInfo o) {
         //noinspection SuspiciousNameCombination
         return Integer.compare(this._y, o._y);
+    }
+
+    @Override
+    public void serializeToDataNode(IDataNode dataNode) {
+        dataNode.set("x", _x);
+        dataNode.set("y", _y);
+        dataNode.set("z", _z);
+        dataNode.set("material", _material);
+        dataNode.set("meta", _data);
+        dataNode.set("light", _light);
+        dataNode.set("sky", _skylight);
+    }
+
+    @Override
+    public void deserializeFromDataNode(IDataNode dataNode) throws UnableToDeserializeException {
+        _x = dataNode.getInteger("x");
+        _y = dataNode.getInteger("y");
+        _z = dataNode.getInteger("z");
+        _material = dataNode.getEnum("material", Material.class);
+        _data = dataNode.getInteger("meta");
+        _light = dataNode.getInteger("light");
+        _skylight = dataNode.getInteger("sky");
+    }
+
+    @Override
+    public void serializeToBytes(GenericsByteWriter writer) throws IOException {
+        writer.write((byte)((_x << 4) | (_y & 0xF)));
+        writer.write((short)_y);
+        writer.write(_material);
+        writer.write((byte)_data);
+        writer.write((byte)((_light << 4) | (_skylight & 0xF)));
+    }
+
+    @Override
+    public void deserializeFromBytes(GenericsByteReader reader) throws IOException, ClassNotFoundException, InstantiationException {
+        int xz = reader.getByte();
+        _x = xz >> 4;
+        _z = xz & 0xF;
+
+        _y = reader.getShort();
+        _material = reader.getEnum(Material.class);
+        _data = reader.getByte();
+
+        int ls = reader.getByte();
+
+        _light = ls >> 4;
+        _skylight = ls & 0xF;
     }
 }
---
+++ src/com/jcwhatever/bukkit/generic/regions/data/WorldInfo.java
@@ -0,0 +1,180 @@
+/*
+ * This file is part of GenericsLib for Bukkit, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) JCThePants (www.jcwhatever.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.jcwhatever.bukkit.generic.regions.data;
+
+import com.jcwhatever.bukkit.generic.file.GenericsByteReader;
+import com.jcwhatever.bukkit.generic.file.GenericsByteWriter;
+import com.jcwhatever.bukkit.generic.file.IGenericsSerializable;
+import com.jcwhatever.bukkit.generic.storage.IDataNode;
+import com.jcwhatever.bukkit.generic.storage.IDataNodeSerializable;
+import com.jcwhatever.bukkit.generic.storage.UnableToDeserializeException;
+import com.jcwhatever.bukkit.generic.utils.PreCon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.World.Environment;
+
+import java.io.IOException;
+import java.util.UUID;
+import javax.annotation.Nullable;
+
+/**
+ * Contains information about a World.
+ */
+public class WorldInfo implements IDataNodeSerializable, IGenericsSerializable {
+
+    private UUID _id;
+    private String _worldName;
+    private Environment _environment;
+
+    /**
+     * Empty Constructor required for {@code IDataNodeSerializable}.
+     */
+    private WorldInfo() {}
+
+    /**
+     * Constructor.
+     *
+     * @param world  The world to get info from.
+     */
+    public WorldInfo (World world) {
+        this(world.getUID(), world.getName(), world.getEnvironment());
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param id           The world id.
+     * @param name         The world name.
+     * @param environment  The world environment.
+     */
+    public WorldInfo (UUID id, String name, Environment environment) {
+        PreCon.notNull(id);
+        PreCon.notNullOrEmpty(name);
+        PreCon.notNull(environment);
+
+        _id = id;
+        _worldName = name;
+        _environment = environment;
+    }
+
+    /**
+     * Get the world unique ID.
+     */
+    public UUID getId() {
+        return _id;
+    }
+
+    /**
+     * Get the world name.
+     */
+    public String getName() {
+        return _worldName;
+    }
+
+    /**
+     * Get the world environment type.
+     */
+    public Environment getEnvironment() {
+        return _environment;
+    }
+
+    /**
+     * Get the world instance.
+     */
+    public World getBukkitWorld() {
+        return Bukkit.getWorld(_id);
+    }
+
+    /**
+     * Determine if the world is loaded.
+     */
+    public boolean isLoaded() {
+        World world = getBukkitWorld();
+        if (world == null)
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Determine if the instance represents the specified
+     * bukkit world.
+     *
+     * @param world  The world to check.
+     */
+    public boolean equalsWorld(@Nullable World world) {
+        return world != null && world.getUID().equals(_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return _id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof WorldInfo &&
+                ((WorldInfo) obj)._id.equals(_id);
+    }
+
+    @Override
+    public void serializeToDataNode(IDataNode dataNode) {
+        dataNode.set("id", _id);
+        dataNode.set("name", _worldName);
+        dataNode.set("environment", _environment);
+    }
+
+    @Override
+    public void deserializeFromDataNode(IDataNode dataNode) throws UnableToDeserializeException {
+        _id = dataNode.getUUID("id");
+        _worldName = dataNode.getString("name");
+        _environment = dataNode.getEnum("environment", Environment.class);
+
+        checkDeserialized(_id);
+        checkDeserialized(_worldName);
+        checkDeserialized(_environment);
+    }
+
+    @Override
+    public void serializeToBytes(GenericsByteWriter writer) throws IOException {
+        writer.write(_id);
+        writer.writeSmallString(_worldName);
+        writer.write(_environment);
+    }
+
+    @Override
+    public void deserializeFromBytes(GenericsByteReader reader) throws IOException, ClassNotFoundException, InstantiationException {
+        _id = reader.getUUID();
+        _worldName = reader.getSmallString();
+        _environment = reader.getEnum(Environment.class);
+    }
+
+    private void checkDeserialized(Object object) throws UnableToDeserializeException {
+        if (object == null) {
+            throw new UnableToDeserializeException();
+        }
+    }
+}
