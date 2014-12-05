c20a7b842e5b61fa3aeba4a994f34255cbe316bb
JCThePants <jcthepants@gmail.com>
2014-12-05 05:54:21 -0800

    add AbstractSetMap

--- src/com/jcwhatever/bukkit/generic/collections/HashSetMap.java
+++ src/com/jcwhatever/bukkit/generic/collections/HashSetMap.java
@@ -27,14 +27,10 @@ package com.jcwhatever.bukkit.generic.collections;
 
 import com.jcwhatever.bukkit.generic.utils.PreCon;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javax.annotation.Nullable;
 
 /**
  * A hash map that uses hash sets to store values.
@@ -42,7 +38,7 @@ import javax.annotation.Nullable;
  * @param <K>  Key type
  * @param <V>  Value type
  */
-public class HashSetMap<K, V> implements Map<K, V> {
+public class HashSetMap<K, V> extends AbstractSetMap<K, V> {
 
     protected Map<K, Set<V>> _map;
 
@@ -64,317 +60,18 @@ public class HashSetMap<K, V> implements Map<K, V> {
         _map = new HashMap<>(size);
     }
 
-    /**
-     * Determine if the map is empty.
-     */
     @Override
-    public boolean isEmpty() {
-        return _map.isEmpty();
+    protected Map<K, Set<V>> getMap() {
+        return _map;
     }
 
-    /**
-     * Clear all items
-     */
     @Override
-    public void clear() {
-        _map.clear();
+    protected Set<V> createSet() {
+        return createSet(10);
     }
 
-    /**
-     * Determine if the map contains the specified key.
-     *
-     * @param key  The key to check.
-     */
     @Override
-    public boolean containsKey(Object key) {
-        PreCon.notNull(key);
-
-        return _map.containsKey(key);
+    protected Set<V> createSet(int size) {
+        return new HashSet<>(size);
     }
-
-    /**
-     * Determine if the map contains the specified value.
-     *
-     * @param value  The value to check.
-     */
-    @Override
-    public boolean containsValue(Object value) {
-        PreCon.notNull(value);
-
-        for (Set<V> stack : _map.values()) {
-
-            //noinspection SuspiciousMethodCalls
-            if (stack.contains(value))
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * Determine if the set represented by the specified
-     * key contains the specified value.
-     *
-     * @param key  The key.
-     */
-    public boolean containsValue(Object key, V value) {
-        PreCon.notNull(key);
-        PreCon.notNull(value);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.get(key);
-        return set != null && set.contains(value);
-    }
-
-    /**
-     * Unsupported.
-     */
-    @Override
-    public Set<Entry<K, V>> entrySet() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Get a value using the specified key.
-     *
-     *  <p>The value returned is the first one returned from the internal hash set.</p>
-     *
-     * @param key  The key to check.
-     */
-    @Override
-    @Nullable
-    public V get(Object key) {
-        PreCon.notNull(key);
-
-        Set<V> set = _map.get(key);
-        if (set != null && !set.isEmpty()) {
-            return set.iterator().next();
-        }
-
-        return null;
-    }
-
-    /**
-     * Get all values associated with the specified key.
-     *
-     * @param key  The key to check.
-     */
-    @Nullable
-    public Set<V> getAll(Object key) {
-        PreCon.notNull(key);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.get(key);
-        if (set == null) {
-            return null;
-        }
-
-        return set;
-    }
-
-
-    /**
-     * Get the maps keys.
-     */
-    @Override
-    public Set<K> keySet() {
-        return _map.keySet();
-    }
-
-    /**
-     * Put a value into the map.
-     *
-     * @param key    The key to use.
-     * @param value  The value to add.
-     */
-    @Override
-    public V put(K key, V value) {
-        PreCon.notNull(key);
-        PreCon.notNull(value);
-
-        Set<V> set = _map.get(key);
-        if (set == null) {
-            set = new HashSet<>(10);
-            _map.put(key, set);
-        }
-
-        set.add(value);
-
-        return value;
-    }
-
-    /**
-     * Unsupported.
-     */
-    @Override
-    public void putAll(Map<? extends K, ? extends V> map) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * All all values in a collection to the specified
-     * key.
-     *
-     * @param key     The key to use.
-     * @param values  The values to add.
-     */
-    public void putAll(K key, Collection<V> values) {
-        PreCon.notNull(key);
-        PreCon.notNull(values);
-
-        if (values.isEmpty())
-            return;
-
-        Set<V> set = _map.get(key);
-        if (set == null) {
-            set = new HashSet<>(10);
-            _map.put(key, set);
-        }
-
-        set.addAll(values);
-    }
-
-    /**
-     * Remove value by key. Removes a value from the internal set
-     * represented by the key.
-     *
-     * @param key  The key.
-     *
-     * @return Returns the removed value, if any.
-     */
-    @Override
-    @Nullable
-    public V remove(Object key) {
-        PreCon.notNull(key);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.get(key);
-        if (set == null) {
-            return null;
-        }
-
-        if (set.isEmpty()) {
-            _map.remove(key);
-            return null;
-        }
-
-        V removed = set.iterator().next();
-        set.remove(removed);
-
-        return removed;
-    }
-
-    /**
-     * Remove a value from the set associated with the key.
-     *
-     * @param key    The key.
-     * @param value  The value.
-     *
-     * @return  True if the collection is modified.
-     */
-    public boolean removeValue(Object key, V value) {
-        PreCon.notNull(key);
-        PreCon.notNull(value);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.get(key);
-        if (set == null) {
-            return false;
-        }
-
-        if (set.isEmpty()) {
-            //noinspection SuspiciousMethodCalls
-            _map.remove(key);
-            return false;
-        }
-
-        return set.remove(value);
-    }
-
-    /**
-     * Remove a value from all sets.
-     *
-     * @param value  The value.
-     *
-     * @return  True if the collection is modified.
-     */
-    public boolean removeValue(V value) {
-        PreCon.notNull(value);
-
-        boolean isModified = false;
-
-        List<Entry<K, Set<V>>> entries = new ArrayList<>(_map.entrySet());
-
-        for (Entry<K, Set<V>> entry : entries) {
-            isModified = isModified || entry.getValue().remove(value);
-            if (entry.getValue().isEmpty()) {
-                _map.remove(entry.getKey());
-            }
-        }
-
-        return isModified;
-    }
-
-    /**
-     * Remove all value of the specified key.
-     *
-     * @param key  The key.
-     *
-     * @return Returns the removed set, if any.
-     */
-    @Nullable
-    public Set<V> removeAll(Object key) {
-        PreCon.notNull(key);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.remove(key);
-        if (set == null) {
-            return null;
-        }
-
-        return set;
-    }
-
-    /**
-     * Get the number of entries in the map.
-     * Or in other terms the number of keys, or
-     * the number of stacks.
-     */
-    @Override
-    public int size() {
-        return _map.size();
-    }
-
-    /**
-     * Get the number of items in the internal stack
-     * represented by the specified key.
-     *
-     * @param key  The key to check.
-     */
-    public int keySize(Object key) {
-        PreCon.notNull(key);
-
-        //noinspection SuspiciousMethodCalls
-        Set<V> set = _map.get(key);
-        if (set == null || set.isEmpty()) {
-            return 0;
-        }
-
-        return set.size();
-    }
-
-    /**
-     * Get all values in the map.
-     */
-    @Override
-    public Collection<V> values() {
-        Collection<Set<V>> values = _map.values();
-        Set<V> results = new HashSet<V>(values.size());
-
-        for (Set<V> set : values) {
-            results.addAll(set);
-        }
-
-        return results;
-    }
-
 }
