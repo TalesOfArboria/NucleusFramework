87dca5151e9e528568cbb871788cf990395ee39c
JCThePants <jcthepants@gmail.com>
2014-12-08 14:06:22 -0800

    PaginatedBankItems implements IPaginator

--- src/com/jcwhatever/bukkit/generic/items/bank/PaginatedBankItems.java
+++ src/com/jcwhatever/bukkit/generic/items/bank/PaginatedBankItems.java
@@ -26,31 +26,27 @@
 package com.jcwhatever.bukkit.generic.items.bank;
 
 import com.jcwhatever.bukkit.generic.items.bank.BankItem.BankItemStack;
+import com.jcwhatever.bukkit.generic.mixins.IPaginator;
 import com.jcwhatever.bukkit.generic.utils.PreCon;
+
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.UUID;
 
 /**
- * Utility to paginate a players item bank account items for use in an chest view.
+ * Utility to paginate a players item bank account items for use in a chest view.
  */
-public class PaginatedBankItems  {
+public class PaginatedBankItems implements IPaginator<BankItemStack> {
 
     public static final int MAX_PER_PAGE = 6 * 9;
     public static final int MAX_PAGES = 6 * 9;
 
-    private UUID _playerId;
-
-    /**
-     * Constructor.
-     *
-     * @param playerId  The id of the player whose account items will be paginated.
-     */
-    public PaginatedBankItems(UUID playerId) {
-        _playerId = playerId;        
-    }
+    private final UUID _playerId;
+    private final PageStartIndex _start;
+    private int _itemsPerPage = MAX_PER_PAGE;
 
     /**
      * Constructor.
@@ -58,7 +54,17 @@ public class PaginatedBankItems  {
      * @param p  The player whose account items will be paginated.
      */
     public PaginatedBankItems(Player p) {
-        _playerId = p.getUniqueId();        
+        this(p.getUniqueId());
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param playerId  The id of the player whose account items will be paginated.
+     */
+    public PaginatedBankItems(UUID playerId) {
+        _playerId = playerId;
+        _start = PageStartIndex.ONE;
     }
 
     /**
@@ -69,21 +75,30 @@ public class PaginatedBankItems  {
     }
 
     /**
+     * Get the {@code PageStartIndex} constant that defines
+     * the index of page 1.
+     */
+    @Override
+    public PageStartIndex getPageStartIndex() {
+        return _start;
+    }
+
+    /**
      * Get total number of stacks (slots) required by the
      * account.
      */
     public int getTotalItems() {
-        
+
         List<BankItem> bankItems = ItemBankManager.getBankItems(_playerId);
         if (bankItems == null)
             return 0;
-        
+
         int size = 0;
-        
+
         for (BankItem bankItem : bankItems) {
             size += (int)Math.ceil((double)bankItem.getQty() / (double)bankItem.getMaxStackSize());
         }
-        
+
         return size;
     }
 
@@ -91,8 +106,8 @@ public class PaginatedBankItems  {
      * Get total number of pages required to display the account items
      * in a 6 x 9 chest.
      */
-    public int getTotalPages () {
-
+    @Override
+    public int getTotalPages() {
         int size = getTotalItems();
 
         int totalPages = (int) Math.ceil((double) size / MAX_PER_PAGE);
@@ -103,20 +118,39 @@ public class PaginatedBankItems  {
     }
 
     /**
-     * Get a specific page of items represented as {@code BankItemStack}'s.
-     *
-     * @param page  The page to get.
+     * Get the max number of items per page.
      */
-    public List<BankItemStack> getPage (int page) {
+    @Override
+    public int getItemsPerPage() {
+        return _itemsPerPage;
+    }
+
+    /**
+     * Set the max number of items per page.
+     *
+     * @param itemsPerPage  The number of items per page.
+     */
+    @Override
+    public void setItemsPerPage(int itemsPerPage) {
+        _itemsPerPage = itemsPerPage;
+    }
+
+    /**
+     * Get a sub list of items from the specified page.
+     *
+     * @param page  The page index.
+     */
+    @Override
+    public List<BankItemStack> getPage(int page) {
         PreCon.greaterThanZero(page);
         PreCon.lessThanEqual(page, MAX_PAGES);
-        
+
         List<BankItem> bankItems = ItemBankManager.getBankItems(_playerId);
         if (bankItems == null)
             return new ArrayList<>(0);
 
         List<BankItemStack> itemStacks = getItemStacks(bankItems);
-        
+
         int total = itemStacks.size();
         int firstItem = page * MAX_PER_PAGE - MAX_PER_PAGE;
         int lastItem = Math.min(firstItem + MAX_PER_PAGE - 1, total - 1);
@@ -136,6 +170,31 @@ public class PaginatedBankItems  {
         return result;
     }
 
+    /**
+     * Get an iterator to iterate over the items of
+     * the specified page.
+     *
+     * @param page  The page index.
+     */
+    @Override
+    public ListIterator<BankItemStack> iterator(int page) {
+        checkPage(page);
+
+        return getPage(page).listIterator();
+    }
+
+    private void checkPage(int page) {
+        switch (_start) {
+            case ZERO:
+                PreCon.positiveNumber(page);
+                break;
+            case ONE:
+                PreCon.greaterThanZero(page);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
 
     private List<BankItemStack> getItemStacks (List<BankItem> bankItems) {
 
@@ -144,13 +203,12 @@ public class PaginatedBankItems  {
         for (BankItem bankItem : bankItems) {
             if (bankItem.getQty() < 1)
                 continue;
-            
+
             List<BankItemStack> items = bankItem.getBankItems();
-            
+
             result.addAll(items);
         }
-        
+
         return result;
     }
-    
 }
