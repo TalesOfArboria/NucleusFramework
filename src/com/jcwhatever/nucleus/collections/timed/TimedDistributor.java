0f74a301fe64572241c5db0057177e116b1925d6
JCThePants <jcthepants@gmail.com>
2015-01-01 15:02:38 -0800

    implement Collection in TimedDistributor

--- src/com/jcwhatever/nucleus/collections/timed/TimedDistributor.java
+++ src/com/jcwhatever/nucleus/collections/timed/TimedDistributor.java
@@ -25,10 +25,12 @@
 package com.jcwhatever.nucleus.collections.timed;
 
 import com.jcwhatever.nucleus.collections.CircularQueue;
+import com.jcwhatever.nucleus.collections.wrappers.AbstractConversionIterator;
 import com.jcwhatever.nucleus.utils.PreCon;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import javax.annotation.Nullable;
 
 /**
@@ -39,50 +41,69 @@ import javax.annotation.Nullable;
  * <p>The current element is passively changed. The {@code #current} method must be
  * invoked at interval for the distributor to be effective.</p>
  */
-public class TimedDistributor<T> {
+public class TimedDistributor<E> implements Collection<E> {
 
-    private CircularQueue<Element<T>> _queue = new CircularQueue<>();
+    private final CircularQueue<Element<E>> _queue = new CircularQueue<>();
+    private final int _defaultTime;
+    private final TimeScale _defaultTimeScale;
+
     private long _nextRotation;
 
     /**
-     * Add an element.
+     * Constructor.
      *
-     * @param element         The element to add.
+     * <p>Default rotation time is 3 seconds.</p>
+     */
+    public TimedDistributor() {
+        this(3, TimeScale.SECONDS);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultTime       The default rotation time.
+     * @param defaultTimeScale  The default time scale.
+     */
+    public TimedDistributor(int defaultTime, TimeScale defaultTimeScale) {
+        PreCon.greaterThanZero(defaultTime);
+        PreCon.notNull(defaultTimeScale);
+
+        _defaultTime = defaultTime;
+        _defaultTimeScale = defaultTimeScale;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param defaultTime       The default rotation time.
+     * @param defaultTimeScale  The default time scale.
+     * @param collection        The initial collection to start with.
+     */
+    public TimedDistributor(int defaultTime, TimeScale defaultTimeScale,
+                            Collection<? extends E> collection) {
+        PreCon.greaterThanZero(defaultTime);
+        PreCon.notNull(defaultTimeScale);
+        PreCon.notNull(collection);
+
+        _defaultTime = defaultTime;
+        _defaultTimeScale = defaultTimeScale;
+
+        for (E value : collection) {
+            add(value, _defaultTime, _defaultTimeScale, false);
+        }
+    }
+
+    /**
+     * Add an element to the head of the distributor.
+     *
+     * @param element    The element to add.
      * @param timeSpan   The number of ticks the element will be current for.
      */
-    public boolean add(T element, int timeSpan, TimeScale timeScale) {
-        PreCon.notNull(element);
+    public boolean add(@Nullable E element, int timeSpan, TimeScale timeScale) {
         PreCon.greaterThanZero(timeSpan);
         PreCon.notNull(timeScale);
 
-        Element<T> timedElement = new Element<>(element, timeSpan, timeScale);
-
-        if (_queue.offerFirst(timedElement)) {
-            _nextRotation = 0;
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Remove an element.
-     *
-     * @param element  The element to remove.
-     */
-    public boolean remove(T element) {
-        PreCon.notNull(element);
-
-        return _queue.removeFirstOccurrence(new Element<T>(element));
-    }
-
-    /**
-     * Determine if the distributor contains an element.
-     *
-     * @param element  The element to check.
-     */
-    public boolean contains(T element) {
-        return _queue.contains(new Element<T>(element));
+        return add(element, timeSpan, timeScale, true);
     }
 
     /**
@@ -92,12 +113,12 @@ public class TimedDistributor<T> {
      * are not started or have been stopped.
      */
     @Nullable
-    public T current() {
+    public E current() {
 
         if (_queue.isEmpty())
             return null;
 
-        Element<T> element;
+        Element<E> element;
         long currentTime = System.currentTimeMillis();
 
         if (_nextRotation == 0) {
@@ -117,22 +138,142 @@ public class TimedDistributor<T> {
             assert element != null;
         }
 
-        return element.value;
+        @SuppressWarnings("unchecked")
+        E value = (E)element.value;
+
+        return value;
     }
 
-    /**
-     * Get all elements.
-     */
-    public Collection<T> getAll() {
-        ArrayList<T> result = new ArrayList<>(_queue.size());
-        for (Element<T> element : _queue) {
-            result.add(element.value);
+    @Override
+    public int size() {
+        return _queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return _queue.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return _queue.contains(new Element<E>(o));
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[_queue.size()];
+        int index = 0;
+        for (Element<E> element : _queue) {
+            result[index] = element.value;
+            index++;
         }
         return result;
     }
 
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        int index = 0;
+        for (Element<E> element : _queue) {
+
+            @SuppressWarnings("unchecked")
+            T1 value = (T1)element.value;
+
+            a[index] = value;
+            index++;
+        }
+        return a;
+    }
+
+    @Override
+    public boolean add(@Nullable E element) {
+        return add(element, _defaultTime, _defaultTimeScale);
+    }
+
+    @Override
+    public boolean remove(@Nullable Object o) {
+        return _queue.removeFirstOccurrence(new Element<E>(o));
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        PreCon.notNull(c);
+
+        for (Object o : c) {
+            if (!contains(o))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Add all items from the collection. The collection is
+     * added to the current head of the distributor and the order
+     * of the collection, if any, is maintained.
+     */
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+
+        LinkedList<E> list = new LinkedList<>(c);
+
+        while (!list.isEmpty()) {
+
+            E element = list.removeLast();
+            add(element);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        PreCon.notNull(c);
+
+        boolean isChanged = false;
+        for (Object o : c) {
+            isChanged = isChanged || remove(o);
+        }
+        return isChanged;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        PreCon.notNull(c);
+
+        Iterator<E> iterator = iterator();
+        while (iterator.hasNext()) {
+            E element = iterator.next();
+
+            if (!c.contains(element))
+                iterator.remove();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        _queue.clear();
+    }
+
+    private boolean add(E element, int timeSpan, TimeScale timeScale, boolean first) {
+
+        Element<E> timedElement = new Element<>(element, timeSpan, timeScale);
+
+        if ((first && _queue.offerFirst(timedElement)) ||
+                (!first && _queue.offerLast(timedElement))) {
+            _nextRotation = 0;
+            return true;
+        }
+
+        return false;
+    }
+
     private static class Element<T> {
-        T value;
+        Object value;
         int timeSpan;
 
         Element(T value, int timeSpan, TimeScale timeScale) {
@@ -140,19 +281,55 @@ public class TimedDistributor<T> {
             this.timeSpan = timeSpan * timeScale.getTimeFactor();
         }
 
-        Element(T value) {
+        Element(Object value) {
             this.value = value;
         }
 
         @Override
         public int hashCode() {
-            return value.hashCode();
+            return value == null
+                    ? super.hashCode()
+                    : value.hashCode();
         }
 
         public boolean equals(Object obj) {
-            return obj instanceof Element
-                    ? ((Element) obj).value.equals(value)
-                    : value.equals(obj);
+            if (obj instanceof Element) {
+                Element other = (Element)obj;
+
+                if (other.value == null && value == null) {
+                    return true;
+                } else if (other.value != null) {
+                    return other.value.equals(value);
+                }
+            }
+            else {
+                if (value == null && obj == null) {
+                    return true;
+                }
+                else if (value != null) {
+                    return value.equals(obj);
+                }
+            }
+            return false;
+        }
+    }
+
+    private class Itr extends AbstractConversionIterator<E, Element<E>> {
+
+        Iterator<Element<E>> iterator = _queue.iterator();
+
+        @Override
+        protected E getElement(Element<E> trueElement) {
+
+            @SuppressWarnings("unchecked")
+            E result = (E)trueElement.value;
+
+            return result;
+        }
+
+        @Override
+        protected Iterator<Element<E>> getIterator() {
+            return iterator;
         }
     }
 }
