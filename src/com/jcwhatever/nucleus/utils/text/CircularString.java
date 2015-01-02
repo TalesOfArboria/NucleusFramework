2b10991b3baa822c06fd839193819c6a0fd7608a
JCThePants <jcthepants@gmail.com>
2015-01-02 01:23:44 -0800

    minor fixes, add #setChar method
    allow negative and out of bounds indexes when retrieving or setting a character

--- src/com/jcwhatever/nucleus/utils/text/CircularString.java
+++ src/com/jcwhatever/nucleus/utils/text/CircularString.java
@@ -81,7 +81,11 @@ public class CircularString implements CharSequence {
      * Reset the rotation back to 0.
      */
     public void reset() {
+        if (_rotation == 0)
+            return;
+
         _rotation = 0;
+        _hasResult = false;
     }
 
     /**
@@ -98,9 +102,9 @@ public class CircularString implements CharSequence {
      */
     public void rotateLeft(int amount) {
 
-        _rotation += amount;
+        _rotation = getCorrectIndex(_rotation + amount);
 
-        correctRotation();
+        _hasResult = false;
     }
 
     /**
@@ -110,9 +114,22 @@ public class CircularString implements CharSequence {
      */
     public void rotateRight(int amount) {
 
-        _rotation -= amount;
+        _rotation = getCorrectIndex(_rotation - amount);
 
-        correctRotation();
+        _hasResult = false;
+    }
+
+    /**
+     * Set the character at the specified position. The position
+     * is relative to the current rotation.
+     *
+     * <p>The index can be a negative or out of bounds number.</p>
+     *
+     * @param index  The index.
+     * @param ch     The replacement character.
+     */
+    public void setChar(int index, char ch) {
+        _string[getCorrectIndex(_rotation + index)] = ch;
     }
 
     @Override
@@ -120,14 +137,26 @@ public class CircularString implements CharSequence {
         return _string.length;
     }
 
+    /**
+     * Set the character at the specified index relative
+     * to the current rotation.
+     *
+     * <p>The index can be a negative or out of bounds number.</p>
+     *
+     * @param index  The index.
+     */
     @Override
     public char charAt(int index) {
-        PreCon.positiveNumber(index, "index");
-        PreCon.lessThan(index, _string.length, "index");
-
-        return _string[(_rotation + index) % _string.length];
+        return _string[getCorrectIndex(_rotation + index)];
     }
 
+    /**
+     * Get a new {@code CircularString} whose characters are
+     * a sub-sequence of characters from the current.
+     *
+     * @param start  The start index of the sequence relative to the current rotation.
+     * @param end    The end index of the sequence relative to the current rotation.
+     */
     @Override
     public CircularString subSequence(int start, int end) {
         PreCon.positiveNumber(start, "start");
@@ -139,15 +168,31 @@ public class CircularString implements CharSequence {
         CircularString result = new CircularString();
         result._string = new char[end - start];
 
-        System.arraycopy(_string, start, result._string, 0, end - start);
+        updateResult();
+
+        System.arraycopy(_result, start % _string.length, result._string, 0, (end - start));
         return result;
     }
 
     @Override
     public String toString() {
 
+        updateResult();
+
+        return new String(_result);
+    }
+
+    private int getCorrectIndex(int index) {
+
+        return index < 0
+                ? _string.length - ((-index) % _string.length)
+                : index % _string.length;
+    }
+
+    private void updateResult() {
+
         if (_hasResult)
-            return new String(_result);
+            return;
 
         if (_result == null || _result.length != _string.length)
             _result = new char[_string.length];
@@ -157,19 +202,5 @@ public class CircularString implements CharSequence {
         }
 
         _hasResult = true;
-
-        return new String(_result);
-    }
-
-    private void correctRotation() {
-        if (_rotation < 0) {
-            _rotation = ((-_rotation) % _string.length) + _string.length;
-            _rotation = _string.length - _rotation;
-        }
-        else {
-            _rotation = _rotation % _string.length;
-        }
-
-        _hasResult = false;
     }
 }
