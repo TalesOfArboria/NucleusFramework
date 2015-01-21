45bc2d60581dd7a8a08a1848dca6ac3f22e6dd3b
JCThePants <JCThePants@gmail.com>
2015-01-21 00:18:58 -0800

    remove IViewFactory/IViewTrigger's
    let plugins implement own triggers

---
+++ tests/com/jcwhatever/nucleus/views/anvil/_AnvilTestSuite.java
@@ -0,0 +1,12 @@
+package com.jcwhatever.nucleus.views.anvil;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AnvilViewTest.class,
+        FilteredAnvilView.class
+})
+public class _AnvilTestSuite {
+}
